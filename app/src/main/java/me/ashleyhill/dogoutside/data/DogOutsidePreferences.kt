75b2eae5e25027cce83a6d6b4b00b794890a2fb6
Ashley Hill <ahill@thisisgrow.com>
2018-05-17 17:25:50 -0400

    Switch to using string-array

--- app/src/main/java/me/ashleyhill/dogoutside/data/DogOutsidePreferences.kt
+++ app/src/main/java/me/ashleyhill/dogoutside/data/DogOutsidePreferences.kt
@@ -9,7 +9,18 @@ import android.preference.PreferenceManager
 
 class DogOutsidePreferences {
     fun getDogName(context: Context, sharedPreferences: SharedPreferences): String {
-             val dogName = sharedPreferences.getString(context.getString(R.string.pref_dog_name_key), "")
+        val dogName = sharedPreferences.getString(context.getString(R.string.pref_dog_name_key), "")
        return context.getString(R.string.dog_title, dogName)
+    }
+
+    fun setDogStatus(context: Context, sharedPreferences: SharedPreferences, string: String) {
+        with(sharedPreferences.edit()) {
+            putString(context.getString(R.string.pref_dog_status_key), string)
+            commit()
+        }
+    }
+
+    fun getDogStatus(context: Context, sharedPreferences: SharedPreferences): String {
+        return sharedPreferences.getString(context.getString(R.string.pref_dog_status_key), context.getString(R.string.pref_dog_status_default))
     }
 }
